6b3bf07bc43fec14f52b2297effd7ce48c71d34c
zhjm <943257156@qq.com>
2019-11-01 14:50:23 +0800

    '打包'

--- App.js
+++ App.js
@@ -9,10 +9,14 @@
 import React from 'react';
 import './src/global/index'
 import MainComponent from './src/page';
+import SafeAreaView from 'react-native-safe-area-view';
 
 const App: () => React$Node = () => {
   return (
     <MainComponent />
+    // <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+    //   <MainComponent />
+    // </SafeAreaView>
   );
 };
 
--- src/page/home/index.js
+++ src/page/home/index.js
@@ -2,10 +2,12 @@ import {
   View,
   SafeAreaView,
   ScrollView,
-  Text
+  Text,
+  FlatList
 } from "react-native";
 import React, { Component } from "react";
 import Head from '../../components/head'
+import { ListItem } from "react-native-elements";
 // import Editor from '../../components/editor'
 
 class Home extends Component {
@@ -36,39 +38,68 @@ class Home extends Component {
 
 class ListView extends Component {
   constructor(props) { 
+    super(props)
     this.state = {
-      itemView: []
+      itemView: [],
+      contentList: []
     }
   }
-  componentWillMount() { 
+  // componentWillMount() { 
+  //   this.renderItem()
+  // }
+  componentDidMount() { 
     this.renderItem()
   }
+
   renderItem() { 
     const ItemView = []
     local.get({ key: 'content' }).then(res => {
-      console.log('--res', res)
-      res.map(item => {
-        const key = Object.keys(item)
-        ItemView.push(
-          <View>
-            <View>{key[0]}</View>
-            <View>{item[key[0]]}</View>
-          </View>
-        )
-      })
       this.setState({
-        itemView: ItemView
+        contentList: res
       })
+      // res.map((item, index) => {
+      //   const key = Object.keys(item)
+      //   ItemView.push(
+      //     <ListItem
+      //       key={index}
+      //       title={key[0]}
+      //       subtitle={item[key[0]]}
+      //       bottomDivider
+      //       chevron
+      //     />
+      //   )
+      // })      
+      // this.setState({
+      //   itemView: ItemView
+      // })
     }).catch(err => { 
       console.log(err)
     })
   }
   render() {
-    console.log('itemView', this.state.itemView)
+    const keyExtractor = (item, index) => index.toString()
+    const itemView = ({ item }) => {
+      const key = Object.keys(item)
+      return (
+        <ListItem
+          key={key}
+          title={key[0]}
+          subtitle={item[key[0]]}
+          bottomDivider
+          chevron
+        />
+      )
+    }
     return (
-      <SafeAreaView>
-        <ScrollView>{this.state.itemView}</ScrollView>
-      </SafeAreaView>      
+      // <View>{this.state.itemView}</View>
+      <FlatList
+        keyExtractor={keyExtractor}
+        data={this.state.contentList}
+        renderItem={itemView}
+      />
+      // <SafeAreaView>
+      //   <ScrollView>{this.state.itemView}</ScrollView>
+      // </SafeAreaView>      
     )
   }
 }
